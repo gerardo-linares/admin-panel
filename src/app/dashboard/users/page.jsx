43bb60ac1c287fc348570b3918e0d875e3215517
gerardo linares <geracoder@gmail.com>
2024-03-08 18:57:48 -0300

    Add user page design

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,5 +1,65 @@
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Nombre</td>
+            <td>Correo</td>
+            <td>Fecha</td>
+            <td>Rol</td>
+            <td>Estado</td>
+            <td>Acción</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/perfil.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Pepe
+              </div>
+            </td>
+            <td>pepe@mail</td>
+            <td>03.30.2024</td>
+            <td>Admin</td>
+            <td>Activo</td>
+            <td>
+              <div className={`${styles.buttons}`}>
+                <Link href="">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    Ver
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Borrar
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default UsersPage;
